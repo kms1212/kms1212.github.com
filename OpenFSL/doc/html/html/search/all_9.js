53fde90e72a875c3e12a25263859ba7a3ddae0d2
kms1212 <kms1212g@gmail.com>
2021-06-24 23:09:57 +0900

    Delete OpenFSL/doc/html/html directory

--- OpenFSL/doc/html/html/search/all_9.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['mbr_2ecpp_144',['mbr.cpp',['../mbr_8cpp.html',1,'']]],
-  ['mbr_2eh_145',['mbr.h',['../mbr_8h.html',1,'']]],
-  ['mkdir_146',['mkdir',['../classopen_f_s_l_1_1_f_s___f_a_t32.html#a5a019dcacc539c2d1f6790d64951b516',1,'openFSL::FS_FAT32']]]
-];
--- OpenFSL/doc/html/html/search/files_5.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['sector_2ecpp_214',['sector.cpp',['../sector_8cpp.html',1,'']]],
-  ['sector_2eh_215',['sector.h',['../sector_8h.html',1,'']]]
-];
