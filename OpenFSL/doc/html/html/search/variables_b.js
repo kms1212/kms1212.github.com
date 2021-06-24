07918287d4fd3938da7ef127863525c9f7aea7c9
kms1212 <kms1212g@gmail.com>
2021-06-24 14:08:10 +0000

    OpenFSL Document update

---
+++ OpenFSL/doc/html/html/search/variables_b.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['sector_5fcount_342',['sector_count',['../classopen_f_s_l_1_1_sector.html#ab94045969a374c777e63e600b2b9400c',1,'openFSL::Sector']]],
+  ['sectorpercluster_343',['sectorPerCluster',['../classopen_f_s_l_1_1_f_s___f_a_t32.html#afeb14dbebb5b0fe08cd77f2af41338c5',1,'openFSL::FS_FAT32']]],
+  ['seeklocation_344',['seekLocation',['../classopen_f_s_l_1_1_f_a_t32___file.html#a02bcd13dce910896737cbb417affaea2',1,'openFSL::FAT32_File']]]
+];
